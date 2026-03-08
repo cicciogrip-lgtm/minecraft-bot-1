8dd4399a64c8cb2061bb0947e87f9dc7fa384af8
cicciogrip-lgtm <cicciogrip@gmail.com>
2026-03-08 23:13:02 +0100

    Refactor bot.js to simplify antiAFK logic

--- bot.js
+++ bot.js
@@ -1,59 +1,31 @@
 const bedrock = require('bedrock-protocol')
 
+function startBot(){
+
 const bot = bedrock.createClient({
   host: 'Valkyrie1.aternos.me',
   port: 28603,
   username: 'MobileBot',
-  offline: true,
-
-  skinData: {
-    DeviceOS: 1, // Android
-    DeviceId: "123456789",
-    PlatformOnlineId: "",
-    SelfSignedId: "00000000-0000-0000-0000-000000000000",
-    ThirdPartyName: "MobileBot",
-    ThirdPartyNameOnly: true
-  }
+  offline: true
 })
 
 bot.on('spawn', () => {
   console.log("Bot entrato nel server")
-
-  setTimeout(() => {
-    antiAFK()
-  }, 10000)
 })
-
-function antiAFK() {
-
-  setInterval(() => {
-
-    try {
-
-      bot.queue('player_auth_input', {
-        pitch: 0,
-        yaw: Math.random() * 360,
-        position: bot.entity.position,
-        move_vector: { x: 0, z: 0 },
-        head_yaw: Math.random() * 360,
-        input_data: {},
-        tick: BigInt(Date.now())
-      })
-
-    } catch {}
-
-  }, 30000)
-
-}
 
 bot.on('text', (packet) => {
   console.log(`[CHAT] ${packet.source_name}: ${packet.message}`)
 })
 
 bot.on('disconnect', () => {
-  console.log("Bot disconnesso dal server")
+  console.log("Disconnesso, riconnessione tra 5 secondi...")
+  setTimeout(startBot, 5000)
 })
 
 bot.on('error', (err) => {
   console.log("Errore:", err)
 })
+
+}
+
+startBot()
